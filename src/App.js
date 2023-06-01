06d1ba3ad4b72c05f5af76b6ad049c8645f3835a
xSStephen <pufuletidinamici@gmail.com>
2023-06-01 12:57:09 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Helmet } from "react-helmet";
+import Chat from "./modules/Chat";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Chat App</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+
+      <Chat />
+    </div>
+  );
 }
 
 export default App;
--- src/modules/Chat.jsx
+++ src/modules/Chat.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { AiOutlineSend } from "react-icons/ai";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <div className="w-full">
+      <div>
+        <div>
+          <form action="">
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <AiOutlineSend size={25} />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
